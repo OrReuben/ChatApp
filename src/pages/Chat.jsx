e812d7620c9dff9e2721502ec5961eb6ed9dfe97
OrReuben <orreuven1243@gmail.com>
2022-11-25 23:54:07 +0200

    fixed responsive

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -112,12 +112,11 @@ const Container = styled.div`
       grid-template-columns: 40% 60%;
     }
     @media screen and (max-width: 600px) {
-      height: 90vh;
+      height: 85vh;
       width: 95vw;
       background-color: #00000076;
       display: grid;
       grid-template-columns: 100%;
-      margin-bottom: 5vh;
     }
   }
 `;
